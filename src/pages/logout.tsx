46781d0dbbebc1c8a65137d2b8048c98fa6650df
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-05 05:12:44 -0700

    Update logout.tsx

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-void */
 import { signOut } from "next-auth/client";
+import { useEffect } from "react";
 
-export default async function Logout(): Promise<JSX.Element> {
-	await signOut();
-	window.location.href = "/";
-
-	return <>Signing Out</>;
+export default function Logout(): JSX.Element {
+	useEffect(() => {
+		void signOut();
+	}, []);
+	return <>Signing Out{(window.location.href = "/")}</>;
 }
