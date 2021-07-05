dc80d307fd89a54c941eec8d92f6f8b4f51301bb
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-05 05:16:37 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
+/* eslint-disable no-void */
 import { Box, Flex, Image } from "@chakra-ui/react";
 import Button from "@components/button";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
+import { useRouter } from "next/router";
 
 export default function Home(): JSX.Element {
+	const router = useRouter();
 	return (
 		<Container bg="bg.secondary">
 			<ContainerInside>
 				<Flex justify="center" align="center">
 					<Box>
-						<Button>Open Dashboard</Button>
+						<Button
+							onClick={() => {
+								void router.push("/dashboard");
+							}}
+						>
+							Open Dashboard
+						</Button>
 					</Box>
 					<Box>
 						<Image src="/undraw/game.svg" w={400} />
