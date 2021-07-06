5eb456f86a5666f855e92f18ebca7ba774b0874e
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-06 04:05:21 -0700

    LESS GOO

--- src/components/dashboard/leaderboards/types.tsx
+++ src/components/dashboard/leaderboards/types.tsx
@@ -4,55 +4,174 @@ import {
 	Box,
 	CircularProgress,
 	CircularProgressLabel,
+	Divider,
 	Flex,
+	Image,
 	Text,
 } from "@chakra-ui/react";
 
-export function Votes({ user, leaderboards, setLeaderboards }: any) {
+export function Votes({ leaderboards }: any) {
 	const newLeaderboards = leaderboards
 		.map((user: any) => {
 			return user;
 		})
 		.sort((a: any, b: any) => b.votes.all - a.votes.all);
-	setLeaderboards(newLeaderboards);
 	return (
-		<Flex justify="center" w={{ base: "100%", md: "inherit" }}>
-			<Box w={{ base: "100%", md: "65px" }}>
-				<Text fontSize="xs" color="text.400">
-					Votes (All)
-				</Text>
-				{user.votes.all}
-			</Box>
-			<Box w={{ base: "100%", md: "85px" }} mx={{ base: 0, md: 3 }}>
-				<Text fontSize="xs" color="text.400">
-					Votes (Monthly)
-				</Text>
-				{user.votes.monthly}
-			</Box>
-		</Flex>
+		<>
+			{newLeaderboards.map((user: any, index: number) => {
+				return (
+					<Flex
+						key={user.tag}
+						justify={{
+							base: "center",
+							md: "space-between",
+						}}
+						flexDir={{ base: "column", md: "row" }}
+						align={{ base: "flex-start", md: "center" }}
+						my={5}
+						textAlign="center"
+					>
+						<Flex justify="center" align="center">
+							<Flex
+								bg={
+									index < 3
+										? index === 2
+											? "silver"
+											: index === 1
+											? "brand.primary2"
+											: "brand.secondary"
+										: "brand.primary"
+								}
+								boxSize="30px"
+								justify="center"
+								align="center"
+								rounded="50%"
+							>
+								<Text>{index + 1}</Text>
+							</Flex>
+
+							<Flex justify="center" mx={3}>
+								<Image
+									src={user.img ? user.img : "/logo.png"}
+									boxSize="50px"
+									rounded="50%"
+								/>
+							</Flex>
+
+							<Text maxW="300px" textAlign="left">
+								{user.tag}
+							</Text>
+						</Flex>
+						<Flex
+							justify="center"
+							w={{ base: "100%", md: "inherit" }}
+						>
+							<Box w={{ base: "100%", md: "65px" }}>
+								<Text fontSize="xs" color="text.400">
+									Votes (All)
+								</Text>
+								{user.votes.all}
+							</Box>
+							<Box
+								w={{ base: "100%", md: "85px" }}
+								mx={{ base: 0, md: 3 }}
+							>
+								<Text fontSize="xs" color="text.400">
+									Votes (Monthly)
+								</Text>
+								{user.votes.monthly}
+							</Box>
+						</Flex>
+
+						<Divider display={{ base: "block", md: "none" }} />
+					</Flex>
+				);
+			})}
+		</>
 	);
 }
 
-export function Rank({ user, leaderboards, setLeaderboards }: any) {
+export function Rank({ leaderboards }: any) {
 	const newLeaderboards = leaderboards
 		.map((user: any) => {
 			return user;
 		})
-		.sort((a: any, b: any) => b.experience - a.experience);
-	setLeaderboards(newLeaderboards);
+		.sort((a: any, b: any) => b.xp - a.xp);
 	return (
-		<Flex justify="center" w={{ base: "100%", md: "inherit" }}>
-			<Box w={{ base: "100%", md: "65px" }}>
-				<Text fontSize="xs" color="text.400">
-					Experience
-				</Text>
-				{user.xp}
-			</Box>
-			<Box w={{ base: "100%", md: "85px" }} mx={{ base: 0, md: 3 }}>
-				<CircularProgress value={user.nextLvlPercent} color="green.400">
-					<CircularProgressLabel>{user.lvl}</CircularProgressLabel>
-				</CircularProgress>
-			</Box>
-		</Flex>
+		<>
+			{newLeaderboards.map((user: any, index: number) => {
+				return (
+					<Flex
+						key={user.tag}
+						justify={{
+							base: "center",
+							md: "space-between",
+						}}
+						flexDir={{ base: "column", md: "row" }}
+						align={{ base: "flex-start", md: "center" }}
+						my={5}
+						textAlign="center"
+					>
+						<Flex justify="center" align="center">
+							<Flex
+								bg={
+									index < 3
+										? index === 2
+											? "silver"
+											: index === 1
+											? "brand.primary2"
+											: "brand.secondary"
+										: "brand.primary"
+								}
+								boxSize="30px"
+								justify="center"
+								align="center"
+								rounded="50%"
+							>
+								<Text>{index + 1}</Text>
+							</Flex>
+
+							<Flex justify="center" mx={3}>
+								<Image
+									src={user.img ? user.img : "/logo.png"}
+									boxSize="50px"
+									rounded="50%"
+								/>
+							</Flex>
+
+							<Text maxW="300px" textAlign="left">
+								{user.tag}
+							</Text>
+						</Flex>
+						<Flex
+							justify="center"
+							w={{ base: "100%", md: "inherit" }}
+						>
+							<Box w={{ base: "100%", md: "65px" }}>
+								<Text fontSize="xs" color="text.400">
+									Experience
+								</Text>
+								{user.xp}
+							</Box>
+							<Box
+								w={{ base: "100%", md: "85px" }}
+								mx={{ base: 0, md: 3 }}
+							>
+								<CircularProgress
+									value={user.nextLvlPercent}
+									color="green.400"
+								>
+									<CircularProgressLabel>
+										{user.lvl}
+									</CircularProgressLabel>
+								</CircularProgress>
+							</Box>
+						</Flex>
+
+						<Divider display={{ base: "block", md: "none" }} />
+					</Flex>
+				);
+			})}
+		</>
 	);
 }
--- src/pages/dashboard/leaderboards.tsx
+++ src/pages/dashboard/leaderboards.tsx
@@ -3,7 +3,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getLeaderboards } from "@api/server";
-import { Box, Divider, Flex, Image, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Layout from "@components/dashboard/layout";
 import { Rank, Votes } from "@components/dashboard/leaderboards/types";
 import { getSession } from "next-auth/client";
@@ -48,32 +48,23 @@ export default function Custom({
 	console.log(leader);
 	guild_id = BigInt(guild_id);
 	const [sort, setSort] = useState(options[0]);
-	const [leaderboards, setLeaderboards] = useState(
-		leader.lbAll
-			.map((user: any) => {
-				return {
-					img: user?.img,
-					tag: user?.tag,
-					votes: {
-						all: user?.votes?.all,
-						monthly: user?.votes?.month,
-					},
-					tokens: user?.tokens,
-					lvl: user?.guilds[guild_id]?.lvl,
-					xp: user?.guilds[guild_id]?.xp,
-					donated: user?.guilds[guild_id]?.donatedTokens,
-					nextLvl: user?.guilds[guild_id]?.nextLvlPercent,
-					messages: user?.guilds[guild_id]?.messages?.all,
-				};
-			})
-			.sort(
-				(
-					a: any,
-					b: any
-				) => {
-					return b.xp - a.xp;
-				}
-			)
+	const [leaderboards] = useState(
+		leader.lbAll.map((user: any) => {
+			return {
+				img: user?.img,
+				tag: user?.tag,
+				votes: {
+					all: user?.votes?.all,
+					monthly: user?.votes?.month,
+				},
+				tokens: user?.tokens,
+				lvl: user?.guilds[guild_id]?.lvl,
+				xp: user?.guilds[guild_id]?.xp,
+				donated: user?.guilds[guild_id]?.donatedTokens,
+				nextLvl: user?.guilds[guild_id]?.nextLvlPercent,
+				messages: user?.guilds[guild_id]?.messages?.all,
+			};
+		})
 	);
 
 	useEffect(() => {
@@ -91,67 +82,11 @@ export default function Custom({
 					styles={customStyles}
 					isSearchable={false}
 				/>
-				{leaderboards.map((user: any, index: number) => {
-					return (
-						<Flex
-							key={user.tag}
-							justify={{
-								base: "center",
-								md: "space-between",
-							}}
-							flexDir={{ base: "column", md: "row" }}
-							align={{ base: "flex-start", md: "center" }}
-							my={5}
-							textAlign="center"
-						>
-							<Flex justify="center" align="center">
-								<Flex
-									bg={
-										index < 3
-											? index === 2
-												? "silver"
-												: index === 1
-												? "brand.primary2"
-												: "brand.secondary"
-											: "brand.primary"
-									}
-									boxSize="30px"
-									justify="center"
-									align="center"
-									rounded="50%"
-								>
-									<Text>{index + 1}</Text>
-								</Flex>
-
-								<Flex justify="center" mx={3}>
-									<Image
-										src={user.img ? user.img : "/logo.png"}
-										boxSize="50px"
-										rounded="50%"
-									/>
-								</Flex>
-
-								<Text maxW="300px" textAlign="left">
-									{user.tag}
-								</Text>
-							</Flex>
-							{sort.value === "rank" ? (
-								<Rank
-									user={user}
-									leaderboards={leaderboards}
-									setLeaderboards={setLeaderboards}
-								/>
-							) : sort.value === "votes" ? (
-								<Votes
-									user={user}
-									leaderboards={leaderboards}
-									setLeaderboards={setLeaderboards}
-								/>
-							) : null}
-							<Divider display={{ base: "block", md: "none" }} />
-						</Flex>
-					);
-				})}
+				{sort.value === "rank" ? (
+					<Rank leaderboards={leaderboards} />
+				) : (
+					<Votes leaderboards={leaderboards} />
+				)}
 			</Box>
 		</Layout>
 	);
@@ -186,10 +121,3 @@ export async function getServerSideProps(context: any) {
 
 	return { props: { session, leader, guild_id } };
 }
-
-/* <Box w={{ base: "100%", md: "50px" }}>
-									<Text fontSize="xs" color="text.400">
-										Tokens
-									</Text>
-									{user.tokens}
-								</Box> */
