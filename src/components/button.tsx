26467505adb8cdbe4706874fbc7432edcf1db807
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-05 22:06:44 -0700

    Editing

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -9,16 +9,16 @@ export default function StyledButton(props: any): JSX.Element {
 			{...props}
 			as="button"
 			// lineHeight="1.2"
-			transition="all 0.2s ease"
+			transition="all 0.1s ease"
 			px="16px"
 			py="4px"
-			borderRadius="10px"
+			borderRadius="md"
 			fontSize="16px"
 			fontWeight="semibold"
 			bg="brand.primary"
 			_hover={{ bg: "brand.secondary" }}
 			_active={{
-				transform: "scale(0.95)",
+				transform: "scale(0.90)",
 			}}
 		>
 			{props.children}
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,76 +1,87 @@
+/* eslint-disable no-void */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable no-bitwise */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getGuilds } from "@api/discord";
-import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image } from "@chakra-ui/react";
 import Button from "@components/button";
 import Layout from "@components/dashboard/layout";
 import { setCookie } from "@lib/cookie";
 import { getSession } from "next-auth/client";
+import { useRouter } from "next/router";
 import { DiscordUser } from "types";
 
 export default function Index({
 	session,
 	guilds,
+	guild_id,
 }: {
+	guild_id: string;
 	guilds: any;
 	session: DiscordUser;
 }): JSX.Element {
+	const router = useRouter();
 	return (
 		<Layout session={session}>
-			{guilds ? (
-				<Box maxW="600px" w="100%">
-					{guilds.map((guild: any) => {
-						if (guild.permissions & (1 << 3)) {
-							return (
+			<Box maxW="600px" w="100%">
+				{guilds.map((guild: any) => {
+					if (guild.permissions & (1 << 3)) {
+						return (
+							<Flex
+								justify="space-between"
+								align="center"
+								my={5}
+								flexDir={{
+									base: "column",
+									md: "row",
+								}}
+								key={guild.id}
+							>
 								<Flex
-									justify="space-between"
 									align="center"
-									my={5}
 									flexDir={{
 										base: "column",
-										md: "row",
+										sm: "row",
 									}}
+									justify="center"
 								>
-									<Flex
-										align="center"
-										flexDir={{
-											base: "column",
-											sm: "row",
-										}}
-										justify="center"
-									>
-										<Image
-											src={`https://cdn.discordapp.com/icons/${
-												guild.id
-											}/${guild.icon}.${
-												guild.icon.startsWith("a_")
-													? "gif"
-													: "png"
-											}`}
-											w={20}
-											rounded="50%"
-											mr={{ base: 0, sm: 5 }}
-										/>
-										<Text>{guild.name}</Text>
-									</Flex>
+									<Image
+										src={`https://cdn.discordapp.com/icons/${
+											guild.id
+										}/${guild.icon}.${
+											guild.icon.startsWith("a_")
+												? "gif"
+												: "png"
+										}`}
+										w={20}
+										rounded="50%"
+										mr={{ base: 0, sm: 5 }}
+									/>
+									<Heading size="md" my={5}>
+										{guild.name}
+									</Heading>
+								</Flex>
+
+								{guild.id === guild_id ? (
+									<Heading size="lg">Editing</Heading>
+								) : (
 									<Button
 										onClick={() => {
 											setCookie("guild", guild.id, 30);
+											
+											void router.push("");
 										}}
 									>
-										Edit guild
+										Edit Guild
 									</Button>
-								</Flex>
-							);
-						}
-					})}
-				</Box>
-			) : (
-				<Heading>You are being rate limited</Heading>
-			)}
+								)}
+							</Flex>
+						);
+					}
+				})}
+			</Box>
 		</Layout>
 	);
 }
@@ -84,12 +95,14 @@ export async function getServerSideProps(context: any) {
 		});
 		context.res.end();
 	}
+
 	// @ts-ignore
 	const guilds = await getGuilds(session.accessToken);
 	return {
 		props: {
 			session,
 			guilds,
+			guild_id: context.req.cookies.guild,
 		},
 	};
 }
