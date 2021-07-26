c1e6612e0b21daf41dfeeaa3125fe5d8cfc76c11
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-25 19:17:50 -0700

    Update navlink.tsx

--- src/components/dashboard/nav/navlink.tsx
+++ src/components/dashboard/nav/navlink.tsx
@@ -6,6 +6,7 @@ import {
 	Text,
 	useColorModeValue as mode,
 } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -13,44 +14,47 @@ type NavLinkProps = {
 	isActive?: boolean;
 	label: string;
 	icon: React.ElementType;
+	href: string;
 } & LinkProps;
 
 export function NavLink(props: NavLinkProps): JSX.Element {
-	const { icon, isActive, label, ...rest } = props;
+	const { icon, isActive, label, href, ...rest } = props;
 	const router = useRouter();
 
 	return (
-		<Link
-			bg={router.asPath === props.href ? "brand.primary2" : ""}
-			display="block"
-			py="2"
-			px="3"
-			borderRadius="md"
-			transition="all 0.3s"
-			fontWeight="medium"
-			fontSize="sm"
-			userSelect="none"
-			aria-current={isActive ? "page" : undefined}
-			color={mode("white.700", "white.400")}
-			// _hover={{
-			// 	bg: mode("blue.200", "blue.700"),
-			// 	color: mode("white.900", "white"),
-			// }}
-			_hover={
-				router.asPath === props.href
-					? { color: "brand.primary" }
-					: { color: "brand.secondary" }
-			}
-			// _activeLink={{
-			// 	bg: router.asPath === props.href ? "blue.200" : "",
-			// 	color: "inherit",
-			// }}
-			{...rest}
-		>
-			<HStack spacing="4">
-				<Icon as={icon} fontSize="lg" opacity={0.64} />
-				<Text as="span">{label}</Text>
-			</HStack>
-		</Link>
+		<NextLink href={href} passHref>
+			<Link
+				bg={router.asPath === props.href ? "brand.primary2" : ""}
+				display="block"
+				py="2"
+				px="3"
+				borderRadius="md"
+				transition="all 0.3s"
+				fontWeight="medium"
+				fontSize="sm"
+				userSelect="none"
+				aria-current={isActive ? "page" : undefined}
+				color={mode("white.700", "white.400")}
+				// _hover={{
+				// 	bg: mode("blue.200", "blue.700"),
+				// 	color: mode("white.900", "white"),
+				// }}
+				_hover={
+					router.asPath === props.href
+						? { color: "brand.primary" }
+						: { color: "brand.secondary" }
+				}
+				// _activeLink={{
+				// 	bg: router.asPath === props.href ? "blue.200" : "",
+				// 	color: "inherit",
+				// }}
+				{...rest}
+			>
+				<HStack spacing="4">
+					<Icon as={icon} fontSize="lg" opacity={0.64} />
+					<Text as="span">{label}</Text>
+				</HStack>
+			</Link>
+		</NextLink>
 	);
 }
