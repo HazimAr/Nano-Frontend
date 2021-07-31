62c4ba8c78429a65e42a7d3c81ec3a6fd227497b
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-30 17:45:38 -0700

    Update guild.tsx

--- src/pages/dashboard/guild.tsx
+++ src/pages/dashboard/guild.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { getGuilds } from "@api/discord";
 import Layout from "@components/dashboard/layout";
 import { getSession } from "next-auth/client";
 import { DiscordUser } from "types";
@@ -7,11 +8,27 @@ import { DiscordUser } from "types";
 export default function Custom({
 	session,
 	guild_id,
+	guilds,
 }: {
 	session: DiscordUser;
 	guild_id: string;
+	guilds: any[];
 }): JSX.Element {
-	return <Layout session={session}>Custom</Layout>;
+	let guild: any = {};
+	guilds.forEach((g) => {
+		if (g.id === guild_id && g.permissions & (1 << 8)) {
+			guild = g;
+			return;
+		}
+		if (guilds.indexOf(g) === guilds.length - 1) {
+			// Reach the end of the array, but the guild was not found
+		}
+	});
+	return (
+		<Layout session={session}>
+			{guild.name ? guild.name : "no guild found"}
+		</Layout>
+	);
 }
 
 export async function getServerSideProps(context: any) {
@@ -25,9 +42,12 @@ export async function getServerSideProps(context: any) {
 		return { props: { session } };
 	}
 
+	// @ts-ignore
+	const guilds = await getGuilds(session?.accessToken);
 	return {
 		props: {
 			session,
+			guilds,
 			guild_id: context.req.cookies.guild || null,
 		},
 	};
