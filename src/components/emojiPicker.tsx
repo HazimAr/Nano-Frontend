fbb18f04303631cd2203e45e19f4b159d3d76dbc
Hazim Arafa <hazimarafa80@gmail.com>
2021-08-05 20:28:52 -0700

    Update emojiPicker.tsx

--- src/components/emojiPicker.tsx
+++ src/components/emojiPicker.tsx
@@ -1,6 +1,8 @@
+import { Box, useDisclosure } from "@chakra-ui/react";
 import { Picker } from "emoji-mart";
 import "emoji-mart/css/emoji-mart.css";
 import { useEffect, useState } from "react";
+import Button from "./button";
 
 export default function EmojiPicker({
 	setParentState,
@@ -13,21 +15,41 @@ export default function EmojiPicker({
 	useEffect(() => {
 		setParentState(emoji);
 	}, [emoji]);
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [mouseX, setMouseX] = useState(0);
+	const [mouseY, setMouseY] = useState(0);
 	return (
-		<Picker
-			onSelect={(emoji) => {
-				for (const item of custom) {
-					if (item.imageUrl === emoji.imageUrl) {
-						setEmoji(item);
-						break;
-					}
+		<Box
+			onMouseMove={(e) => {
+				if (!isOpen) {
+					setMouseX(e.clientX);
+					setMouseY(e.clientY);
 				}
 			}}
-			set="twitter"
-			title="Pick your emoji…"
-			emoji="point_up"
-			theme="dark"
-			custom={custom}
-		/>
+		>
+			<Button onClick={isOpen ? onClose : onOpen}>
+				{isOpen ? "Close" : "Open"}
+			</Button>
+			{isOpen ? (
+				<Picker
+					onSelect={(emoji) => {
+						for (const item of custom) {
+							if (item.imageUrl === emoji.imageUrl) {
+								setEmoji(item);
+								break;
+							}
+						}
+					}}
+					set="twitter"
+					title="Pick your emoji…"
+					emoji="point_up"
+					theme="dark"
+					custom={custom}
+					position="absolute"
+					top={mouseY}
+					left={mouseX}
+				/>
+			) : null}
+		</Box>
 	);
 }
