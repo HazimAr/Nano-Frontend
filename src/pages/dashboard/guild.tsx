64ec32c5b6a85b476541d01d188d0ada5ff49967
IsamarZhu <51845693+IsamarZhu@users.noreply.github.com>
2021-08-10 00:45:27 -0700

    Revert "Update guild.tsx"
    This reverts commit 1ffb4c6330335f9a37aa8ef10b1e737d776de133.

--- src/pages/dashboard/guild.tsx
+++ src/pages/dashboard/guild.tsx
@@ -28,7 +28,79 @@ export default function Custom({
 	return (
 		<Layout session={session}>
 			<Stack align="center" mt={5} spacing={5}>
-
+				<Flex align="center" w="100%">
+					<Avatar
+						// size="lg"
+						boxSize={{
+							base: "75px",
+							sm: "125px",
+							md: "175px",
+						}}
+						src={`https://cdn.discordapp.com/icons/${
+							discordGuild.id
+						}/${discordGuild.icon}.${
+							discordGuild.icon?.startsWith("a_") ? "gif" : "png"
+						}`}
+						fallbackSrc="/oss.png"
+						rounded="50%"
+						mx={{ base: 0, sm: 5 }}
+						name={discordGuild.name}
+					/>
+					<Heading ml={5}>{discordGuild.name}</Heading>
+					{/* <Box mx={3}>
+						<Level user={serverUser} guild={guildId} size={75} />
+					</Box> */}
+				</Flex>
+				<HStack
+					flexDirection={{ base: "column", sm: "row" }}
+					align={{ base: "center", sm: "flex-start" }}
+					justify={{ base: "center", sm: "flex-start" }}
+					textAlign={{ base: "center", sm: "left" }}
+					w="100%"
+				>
+					<Heading w="100%">Prefix: {mongoGuild.prefix}</Heading>
+					<Heading w="100%">Tokens: {mongoGuild.tokens}</Heading>
+					<Heading w="100%">Xp: {mongoGuild.xp}</Heading>
+					{/* <Text w="100%">
+						Votes: {serverUser.votes.all}{" "}
+						<Button
+							fontSize="6px"
+							onClick={() => {
+								void router.push("/vote");
+							}}
+						>
+							Vote
+						</Button>
+					</Text> */}
+				</HStack>
+				<Grid
+					templateColumns={{
+						base: "repeat(1, 1fr)",
+						sm: "repeat(2, 1fr)",
+					}}
+					gap={5}
+				>
+					<Panel
+						name="Enable / Disable Commands"
+						description="Click on a switch to activate and deactivate certain command nano commands."
+						href="/commands"
+					/>
+					<Panel
+						name="Custom Commands"
+						description="Set a custom command so when you type -myCustomCommand the bot will respond with a customizable message."
+						href="/custom"
+					/>
+					<Panel
+						name="Reaction Roles"
+						description="Create a customizable message that when users will react to they will recieve a specified role."
+						href="/reaction"
+					/>
+					<Panel
+						name="Timers"
+						description="Add Timers to specific channels in your server. On an interval the bot will send a message in that channel."
+						href="/timers"
+					/>
+				</Grid>
 			</Stack>
 		</Layout>
 	);
