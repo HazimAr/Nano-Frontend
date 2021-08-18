5e0c9362986f9eea8c59076b6e20e3fedd116521
Hazim Arafa <hazimarafa80@gmail.com>
2021-08-17 18:23:30 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ const META: MetaType = {
 
 const SERVER_URL = IS_PRODUCTION
 	? "https://nano-osu.teamdragonsden.com"
-	: " https://4a4d05936139.ngrok.io";
+	: "https://67635a4c8d10.ngrok.io";
 
 const DISCORD_BASE_URL = "https://discord.com/api/v9";
 
