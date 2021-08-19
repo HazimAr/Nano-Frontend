73660329745760601d0fab3c80c54274f4c3270a
Joshua <icjosh@gmail.com>
2021-08-19 17:21:46 -0500

    role_playing (all 3 sections)
    role_playing (all 3 sections)

---
+++ src/pages/dashboard/[id]/role_playing.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { defaultGuildPost } from '@api/server';
+import { Heading, Stack } from '@chakra-ui/react';
+import { CommandSection } from '@components/dashboard/commands/_commands';
+import Layout from '@components/dashboard/layout';
+import { getSession } from 'next-auth/client';
+import { DiscordUser } from 'types';
+
+export default function Role_Playing({ session, data, guild_id }: { session: DiscordUser; data: any; guild_id: string }): JSX.Element {
+	// console.log(data);
+	const { commands } = data;
+	const [positive, neutral, negative] = commands;
+
+	return (
+		<Layout session={session}>
+			<Stack spacing={3} flexDir="column" maxW="1200px" w="100%">
+				<Heading textAlign="center">Enable and Disable Role Playing Commands</Heading>
+				<CommandSection title="Positive" commands={positive} />
+				<CommandSection title="Neutral" commands={neutral} />
+				<CommandSection title="Negative" commands={negative} />
+			</Stack>
+		</Layout>
+	);
+}
+
+export async function getServerSideProps(context: any) {
+	const session = await getSession(context);
+	if (!session) {
+		context.res.writeHead(307, {
+			Location: '/',
+		});
+		context.res.end();
+		return { props: { session } };
+	}
+	if (!context.req.url.split('/')[2]) {
+		context.res.writeHead(307, {
+			Location: '/dashboard',
+		});
+		context.res.end();
+		return { props: { session } };
+	}
+
+	const [, , guild_id] = context.req.url.split('/');
+	const data = await defaultGuildPost('role_playing', guild_id, session.accessToken);
+
+	return { props: { session, data, guild_id } };
+}
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,77 +1,74 @@
 /* eslint-disable import/no-default-export */
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
 	colors: {
 		bg: {
-			primary:
-				"hsl(267.15328467153284, 14.285714285714283%, 17.84313725490196%)",
-			secondary:
-				"hsl(267.15328467153284, 7.462686567164178%, 13.137254901960786%)",
+			primary: 'hsl(267.15328467153284, 14.285714285714283%, 17.84313725490196%)',
+			secondary: 'hsl(267.15328467153284, 7.462686567164178%, 13.137254901960786%)',
 		},
 		brand: {
-			error: "#cf6679",
-			secondary: "#f6a",
-			primary: "#7549ac",
-			primary2: "#fab107",
+			error: '#cf6679',
+			secondary: '#f6a',
+			primary: '#7549ac',
+			primary2: '#fab107',
 		},
 		text: {
-			50: "#FAFAFA",
-			100: "#F5F5F5",
-			200: "#EEEEEE",
-			300: "#E0E0E0",
-			400: "#BDBDBD",
-			500: "#9E9E9E",
-			600: "#757575",
-			700: "#616161",
-			800: "#424242",
-			900: "#212121",
-			1000: "#161616",
+			50: '#FAFAFA',
+			100: '#F5F5F5',
+			200: '#EEEEEE',
+			300: '#E0E0E0',
+			400: '#BDBDBD',
+			500: '#9E9E9E',
+			600: '#757575',
+			700: '#616161',
+			800: '#424242',
+			900: '#212121',
+			1000: '#161616',
 		},
-		rounded: "10px",
+		rounded: '10px',
 	},
 	styles: {
 		global: () => ({
 			html: {
-				height: "100%",
+				height: '100%',
 			},
 			body: {
 				fontFamily:
-					"-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
-				color: "white",
-				lineHeight: "base",
+					'-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+				color: 'white',
+				lineHeight: 'base',
 				padding: 0,
 				margin: 0,
-				backgroundColor: "bg.primary",
+				backgroundColor: 'bg.primary',
 			},
 			a: {
-				color: "inherit",
-				textDecoration: "none",
+				color: 'inherit',
+				textDecoration: 'none',
 			},
 			ul: {
-				listStyle: "none",
+				listStyle: 'none',
 			},
-			"&::-webkit-scrollbar": {
-				background: "#000000",
-				width: "0.6em",
+			'&::-webkit-scrollbar': {
+				background: '#000000',
+				width: '0.44em',
 			},
-			"&::-webkit-scrollbar-track": {
+			'&::-webkit-scrollbar-track': {
 				// boxShadow: "inset 0 0 2px #1A1A1D",
-				borderRadius: "0px",
-				background: "brand.primary",
+				borderRadius: '0px',
+				background: 'brand.primary',
 			},
-			"&::-webkit-scrollbar-thumb": {
-				background: "brand.secondary",
-				borderRadius: "50px",
+			'&::-webkit-scrollbar-thumb': {
+				background: 'brand.secondary',
+				borderRadius: '50px',
 			},
 
-			"@font-face": {
-				fontFamily: "Gagalin",
+			'@font-face': {
+				fontFamily: 'Gagalin',
 				src: "url('/fonts/Gagalin.ttf')",
 			},
 		}),
 	},
-
 });
 
 export default theme;
