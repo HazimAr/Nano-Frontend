b8b6ace276aa4ac98026a63918c8df0fe3e966d1
Joshua <icjosh@gmail.com>
2021-08-19 06:06:38 -0500

    cleaner server requests (not done)
    cleaner server requests (not done)

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,68 +1,95 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import axios from "axios";
-import { SERVER_URL } from "config";
-import { getId } from "./discord";
+import axios from 'axios';
+import { SERVER_URL } from 'config';
+import { getId } from './discord';
 
 const config = {
 	timeout: 1000 * 10,
 };
 
+//
+// 🌟 ------------------------------------------------------------------- POST REQUESTS ------------------------------------------------------------------- 🌟 //
+//
+export async function defaultGuildPost(guild_file: string, guild_id: string, authorization: string) {
+	return (await axios.post(`${SERVER_URL}/g/${guild_file}`, { guild_id, authorization: `Bearer ${authorization}` }, config)).data;
+}
+//
+// ----------------------------------------------------------------------------------------------------------------------
+//
 export async function getLeaderboards() {
-	const { data } = await axios.post(`${SERVER_URL}/lb`, config);
-	return data;
+	return await axios.post(`${SERVER_URL}/lb`, config);
 }
 
 export async function getGuildLeaderboards(guild_id) {
-	const { data } = await axios.post(
-		`${SERVER_URL}/lb/guild`,
-		{ guild_id },
-		config
-	);
-	return data;
+	return await axios.post(`${SERVER_URL}/lb/guild`, { guild_id }, config);
 }
 
 export async function getOsuRank(id: string) {
-	const { data } = await axios.post(`${SERVER_URL}/osu/user`, { id }, config);
-
-	return data;
+	return await axios.post(`${SERVER_URL}/osu/user`, { id }, config);
 }
 
 export async function getUser(token: unknown) {
-	const { data } = await axios.post(
-		`${SERVER_URL}/u/profile`,
-		{ authorization: `Bearer ${token}` },
+	return await axios.post(`${SERVER_URL}/u/profile`, { authorization: token }, config);
+}
+
+//
+// -------------------------------- 💠 Guilds 💠 --------------------------------
+//
+
+// export async function getGuildAnime(guild_id: string, token: string) {
+// 	return await defaultGuildPost('anime', guild_id, token);
+// }
+
+export async function getGuildOsu(guild_id: string, token: string) {
+	return await defaultGuildPost('osu', guild_id, token);
+}
+
+export async function getGuildReactionRoles(guild_id: string, token: string) {
+	return await defaultGuildPost('reactionRoles', guild_id, token);
+}
+
+export async function getNanoCommands(guild_id: string, token: string) {
+	return await defaultGuildPost('toggleCommands', guild_id, token);
+}
+
+export async function getCustomCommands(guild_id: string, token: string) {
+	return await defaultGuildPost('customCommands', guild_id, token);
+}
+
+export async function getGuild(guild_id: string, token: string) {
+	return await defaultGuildPost('profile', guild_id, token);
+}
+
+export async function getGuildTimers(guild_id: string, token: string) {
+	return await defaultGuildPost('timers', guild_id, token);
+}
+
+// This 👇 should be using g/guild to get premium 🤔
+
+// export async function getGuildPremium(guild_id: string, token: unknown) {
+// 	return await defaultGuildPost('premium', guild_id, `Bearer ${token}`);
+// }
+
+//
+// 🍎 ------------------------------------------------------------------- PUT REQUESTS ------------------------------------------------------------------- 🍎 //
+//
+export async function updateNanoCommands(guild_id: string, commandsToChange: object, token: string) {
+	const { data } = await axios.put(
+		`${SERVER_URL}/g/toggleCommands`,
+		{
+			guild_id,
+			commandsToChange,
+			authorization: `Bearer ${token}`,
+		},
 		config
 	);
-
 	return data;
 }
 
 export async function loginOsu(token: string) {
 	const id = await getId(token);
-	const { data } = await axios.put(
-		`${SERVER_URL}/osu/newUser`,
-		{ id },
-		config
-	);
-	return data;
-}
-
-export async function getCustomCommands(guild_id: string, token: unknown) {
-	const { data } = await axios.post(
-		`${SERVER_URL}/g/customCommands`,
-		{ guild_id, authorization: `Bearer ${token}` },
-		config
-	);
-	return data;
-}
-
-export async function getGuild(guild_id: string, token: unknown) {
-	const { data } = await axios.post(
-		`${SERVER_URL}/g/profile`,
-		{ guild_id, authorization: `Bearer ${token}` },
-		config
-	);
+	const { data } = await axios.put(`${SERVER_URL}/osu/newUser`, { id }, config);
 	return data;
 }
 
@@ -119,32 +146,7 @@ export async function createTimer(
 	return data;
 }
 
-export async function getGuildTimers(guild_id: string, token: unknown) {
-	const { data } = await axios.post(
-		`${SERVER_URL}/g/timers`,
-		{ guild_id, authorization: `Bearer ${token}` },
-		config
-	);
-	return data;
-}
-
-export async function getGuildPremium(guild_id: string, token: unknown) {
-	const { data } = await axios.post(
-		`${SERVER_URL}/g/premium`,
-		{
-			guild_id,
-			authorization: `Bearer ${token}`,
-		},
-		config
-	);
-	return data;
-}
-
-export async function deleteGuildTimer(
-	guild_id: string,
-	timer_id: string,
-	token: unknown
-) {
+export async function deleteGuildTimer(guild_id: string, timer_id: string, token: unknown) {
 	const { data } = await axios.put(
 		`${SERVER_URL}/g/timers/delete`,
 
@@ -179,47 +181,6 @@ export async function createReactionRoleMessage(
 			message,
 			enabled,
 			role_rows,
-			authorization: `Bearer ${token}`,
-		},
-		config
-	);
-	return data;
-}
-
-export async function getGuildReactionRoles(guild_id: string, token: unknown) {
-	const { data } = await axios.post(
-		`${SERVER_URL}/g/reactionRoles`,
-		{
-			guild_id,
-			authorization: `Bearer ${token}`,
-		},
-		config
-	);
-	return data;
-}
-
-export async function getNanoCommands(guild_id: string, token: unknown) {
-	const { data } = await axios.post(
-		`${SERVER_URL}/g/toggleCommands`,
-		{
-			guild_id,
-			authorization: `Bearer ${token}`,
-		},
-		config
-	);
-	return data;
-}
-
-export async function updateNanoCommands(
-	guild_id: string,
-	commandsToChange: object,
-	token: string
-) {
-	const { data } = await axios.put(
-		`${SERVER_URL}/g/toggleCommands`,
-		{
-			guild_id,
-			commandsToChange,
 			authorization: `Bearer ${token}`,
 		},
 		config
