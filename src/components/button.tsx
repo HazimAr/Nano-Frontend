a6c9416143351cbc0ccce3f2b157c8d7e2fe6956
Joshua <icjosh@gmail.com>
2021-09-06 02:51:25 -0500

    handle button bg better
    handle button bg better

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,23 +1,19 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/no-default-export */
-import { Box } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react';
 
 export default function Button(props: any): JSX.Element {
 	let bg;
 	let hover;
 	switch (props.type) {
-		default:
-			bg = "brand.primary";
-			hover = "brand.secondary";
+		case 'secondary':
+			hover = 'brand.secondary';
+			bg = 'brand.primary2';
 			break;
-		case "secondary":
-			hover = "brand.secondary";
-			bg = "brand.primary2";
-			break;
-		case "delete":
-			bg = "rgb(237,66,69)";
-			hover = "brand.secondary";
+		case 'delete':
+			bg = 'rgb(237,66,69)';
+			hover = 'brand.secondary';
 			break;
 	}
 	return (
@@ -31,10 +27,10 @@ export default function Button(props: any): JSX.Element {
 			rounded={5}
 			fontSize="16px"
 			fontWeight="semibold"
-			bg={bg}
+			bg={bg ? bg : props.bg}
 			_hover={{ background: hover }}
 			_active={{
-				transform: "scale(0.90)",
+				transform: 'scale(0.90)',
 			}}
 		>
 			{props.children}
