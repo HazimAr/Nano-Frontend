21d17f9c318889253979adab4e0060cba5d59bfc
Joshua <icjosh@gmail.com>
2021-09-10 01:44:39 -0500

    handle progress in layout + package
    handle progress in layout + package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,11 @@
-import ProgressBar from '@badrap/bar-of-progress';
 import { ChakraProvider } from '@chakra-ui/react';
-import { pageview } from '@lib/gtag';
 import theme from '@styles/theme';
 import { META } from 'config';
 import { Provider as NextAuthProvider } from 'next-auth/client';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	const router = useRouter();
-
-	const progress = new ProgressBar({
-		size: 2,
-		color: theme.colors.brand.secondary,
-		delay: 0,
-	});
-
-	useEffect(() => {
-		const handleRouteChange = (url: unknown) => {
-			pageview(url);
-		};
-		router.events.on('routeChangeStart', progress.start);
-		router.events.on('routeChangeComplete', progress.finish);
-		router.events.on('routeChangeError', progress.finish);
-		router.events.on('routeChangeComplete', handleRouteChange);
-		return () => router.events.off('routeChangeComplete', handleRouteChange);
-	}, [router.events]);
-
 	return (
 		<>
 			<Head>
